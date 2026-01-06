3cb8b3b7be6ebf0a6980517211df5ccfa1df68f1
AbdRofiqMm <abdulrofiqmahmud1@gmail.com>
2026-01-06 21:01:42 +0800

    setup structure package

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"log"
-	"net/http"
 
+	"github.com/AbdRofiqMm/go-simple-rest-api/internal/handlers/memberships"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,12 +12,8 @@ func main() {
 	r := gin.Default()
 
 	// Define a simple GET endpoint
-	r.GET("/ping", func(c *gin.Context) {
-		// Return JSON response
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	membershipsHandler := memberships.NewHandler(r)
+	membershipsHandler.RegisterRoute()
 
 	// Start server on port 8080 (default)
 	// Server will listen on 0.0.0.0:8080 (localhost:8080 on Windows)
